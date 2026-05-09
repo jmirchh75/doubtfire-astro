07f7b06bfdbbbb36f5ab1b11d825753ba9e6b46f
jmirchh75 <jmirch@live.com>
2026-05-10 03:40:04 +1000

    fix(config): add site URL for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-
+  site: 'https://ontrackdocumentation.netlify.app',
+  
   server: {
     host: true
   },
